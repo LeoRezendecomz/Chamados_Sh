5e673e05c6a27a47ff6f1bdb3303d1c5519eae2c
Leonardo <161914113+LeoRezendecomz@users.noreply.github.com>
2025-03-13 19:57:55 -0300

    Update supabase.ts

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -1,1 +1,58 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
+export interface Database {
+  public: {
+    Tables: {
+      Chamados_SH: {
+        Row: {
+          id: number
+          chamado_facil: string
+          chamado_sd: string
+          titulo: string
+          data_abertura: string
+          cumpriu_sla: 'Sim' | 'Não'
+          pendencia_retorno: 'Fácil' | 'Nordeste'
+          usuario_resp: string
+          status: 'Aberto' | 'Concluído'
+        }
+        Insert: {
+          id?: number
+          chamado_facil: string
+          chamado_sd: string
+          titulo: string
+          data_abertura: string
+          cumpriu_sla: 'Sim' | 'Não'
+          pendencia_retorno: 'Fácil' | 'Nordeste'
+          usuario_resp: string
+          status: 'Aberto' | 'Concluído'
+        }
+        Update: {
+          id?: number
+          chamado_facil?: string
+          chamado_sd?: string
+          titulo?: string
+          data_abertura?: string
+          cumpriu_sla?: 'Sim' | 'Não'
+          pendencia_retorno?: 'Fácil' | 'Nordeste'
+          usuario_resp?: string
+          status?: 'Aberto' | 'Concluído'
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
